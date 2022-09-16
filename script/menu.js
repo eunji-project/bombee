3df7a2122e64912cb50ac8649e8615a6f380fce6
eunji-project <113660678+eunji-project@users.noreply.github.com>
2022-09-16 14:27:21 +0900

    Add files via upload

---
+++ script/menu.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.nav> li').mouseenter(function(){
+        $('.sub').stop().slideDown();
+    });
+    $('.nav> li').mouseleave(function(){
+        $('.sub').stop().slideUp();
+    });
+});
