8771c08be01262ab06f9690be7ef20045b79b78f
eunji-project <113660678+eunji-project@users.noreply.github.com>
2022-09-19 16:00:29 +0900

    Add files via upload

---
+++ script/popup.js
@@ -0,0 +1,26 @@
+$(function(){
+    $('.cakepop').click(function(){
+        $('.popup').fadeIn();//show()
+    });
+    $('.popup a').click(function(){
+        $('.popup').fadeOut();//hide()
+    });
+});
+
+$(function(){
+    $('.beverage_pop').click(function(){
+        $('.popup2').fadeIn();//show()
+    });
+    $('.popup2 a').click(function(){
+        $('.popup2').fadeOut();//hide()
+    });
+});
+
+$(function(){
+    $('.goods_pop').click(function(){
+        $('.popup3').fadeIn();//show()
+    });
+    $('.popup3 a').click(function(){
+        $('.popup3').fadeOut();//hide()
+    });
+});
