b5673019ad7b51374a3d8acaaae9158657b3c853
eunji-project <113660678+eunji-project@users.noreply.github.com>
2022-09-19 15:56:39 +0900

    Delete script directory

--- script/menu.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.nav> li').mouseenter(function(){
-        $('.sub').stop().slideDown();
-    });
-    $('.nav> li').mouseleave(function(){
-        $('.sub').stop().slideUp();
-    });
-});
